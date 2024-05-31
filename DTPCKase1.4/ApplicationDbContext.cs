78015bda04a6b8a229914efc21d4c228a9c1ed2b
Mike Aranguren <miguel.aranguren.2077@gmail.com>
2024-05-31 01:45:15 -0500

    Add project files.

---
+++ DTPCKase1.4/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTPCKase1._4
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+    }
+}
